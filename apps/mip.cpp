f9a399097565b2d4b4055bfbe82c6afcbb051abc
zdc <3491957218@qq.com>
2023-12-31 14:32:44 +0800

    增加icon

--- apps/mip.cpp
+++ apps/mip.cpp
@@ -6,6 +6,7 @@ int main(int argc,char* argv[])
     QApplication a(argc,argv);
     MainInterface m;
     m.setWindowTitle("MIP");
+    m.setWindowIcon(QIcon("res/icon.png"));
     m.show();
     return a.exec();
 }
