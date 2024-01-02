1b3378ed0ca08704ee45f84c403f2419238d6b55
zdc <3491957218@qq.com>
2024-01-02 11:29:13 +0800

    增加图标备份

--- apps/mip.cpp
+++ apps/mip.cpp
@@ -6,7 +6,7 @@ int main(int argc,char* argv[])
     QApplication a(argc,argv);
     MainInterface m;
     m.setWindowTitle("MIP");
-    m.setWindowIcon(QIcon("res/icon.png"));
+    m.setWindowIcon(QIcon("src/res/icon.png"));
     m.show();
     return a.exec();
 }
